b10a5c5d6b53e75c199f05aabe7dd802841dfa65
Angela Rivas <snowcats001@gmail.com>
2018-12-06 13:27:14 -0700

    Added scroll left and right for before and after controller. And added index presentation method

--- AbstractionApp/Controller/AbstractionViewController.swift
+++ AbstractionApp/Controller/AbstractionViewController.swift
@@ -10,32 +10,88 @@ import UIKit
 
 public class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
 {
-
+    public override func viewDidLoad()
+    {
     super.viewDidLoad()
     dataSource = self
     
-    if let firstViewController = orderedAbstractionViews.first
-    {
-       setViewControllers([firstViewController],
-                          direction: .forward,
-                          animated: true,
-                          completion: nil)
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
 
         // Do any additional setup after loading the view.
+        }
     }
     private lazy var orderedAbstractionViews : [UIViewController] =
     {
         return [
             self.newAbstractionViewController(abstractionLevel: "Swift"),
-            self.newAbstractionViewCOntroller(abstractionLevel: "Block"),
-            self.newAbstractionViewCOntroller(abstractionLevel: "ByteCode"),
-            self.newAbstractionViewCOntroller(abstractionLevel: "Binary"),
-            self.newAbstractionViewCOntroller(abstractionLevel: "LogicalGate")
+            self.newAbstractionViewController(abstractionLevel: "Block"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "LogicalGate")
         ]
     }()
-    private func newAbstractionViewCOntroller(abstractionLevel: String) -> UIViewController
+    private func newAbstractionViewController(abstractionLevel: String) -> UIViewController
     {
-        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstrationLevel)ViewController")
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
+    }
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+            else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+            else
+        {
+            return nil
+        }
+        return orderedAbstractionViews[previousIndex]
+    }
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            else
+        {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < orderedAbstractionViews.count
+            else
+        {
+            return orderedAbstractionViews.first
+        }
+        
+        return orderedAbstractionViews[nextIndex]
+    }
+    public func presentationCount(for pageViewController: UIPageViewController) -> Int
+    {
+        return orderedAbstractionViews.count
+    }
+    public func presentationIndex(for pageViewController: UIPageViewController) -> Int
+    {
+        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = orderedAbstractionViews.index(of: firstViewController)
+            else
+        {
+            return 0
+        }
+        return firstViewControllerIndex
     }
     
 
